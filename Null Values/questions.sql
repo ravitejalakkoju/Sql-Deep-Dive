8143aab152cb7d0ce34c38eb9b1f09f4bae4c514
Ravi Teja Lakkoju <raviteja6820@gmail.com>
2022-10-20 19:56:33 +0530

    null values

---
+++ Null Values/questions.sql
@@ -0,0 +1,13 @@
+
+/*
+* DB: https://www.db-fiddle.com/f/PnGNcaPYfGoEDvfexzEUA/0
+* Question: 
+* Assuming a students minimum age for the class is 15, what is the average age of a student?
+*/
+select avg(coalesce(age,15)) from student;
+/*
+* DB: https://www.db-fiddle.com/f/PnGNcaPYfGoEDvfexzEUA/0
+* Question: 
+* Replace all empty first or last names with a default?
+*/
+select id, coalesce(firstname, 'fallback'), coalesce(lastname, 'lastname') from student
